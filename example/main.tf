d4dc74a710851f4c1546a05b4896012d7375d9ed
Jeremy A. Mowery <Jeremy.Mowery@cms.hhs.gov>
2024-06-12 14:51:36 -0400

    TMP change example cluster name to mowery-test

--- example/main.tf
+++ example/main.tf
@@ -9,8 +9,7 @@ provider "aws" {
 
 module "main-eks" {
   source = "git@github.com:CMS-Enterprise/Energon-Kube.git?ref=3.1.2"
-
-  cluster_custom_name = "temp-test"
+  cluster_custom_name = "mowery-test"
   env                 = "impl"
   gold_image_date     = "2024-07"
   ado                 = "batcave"
