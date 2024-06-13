ce3126d8ce7bf2594c24bf866c93ce23f698543d
Max Rahimi <masood.rahimi@cms.hhs.giv>
2024-06-13 16:41:58 -0400

    fixed fluentbit log name

--- fluentbit.tf
+++ fluentbit.tf
@@ -1,6 +1,6 @@
 #Cloudwatch Log Group
 resource "aws_cloudwatch_log_group" "fluent-bit" {
-  name              = "${local.fluentbit_log_name}-${module.eks.cluster_name}"
+  name              = local.fluentbit_log_name
   retention_in_days = var.fb_log_retention
   kms_key_id        = var.fb_log_encryption ? module.cloudwatch_kms.key_arn : null
   tags              = var.fb_tags
@@ -8,7 +8,7 @@ resource "aws_cloudwatch_log_group" "fluent-bit" {
 
 resource "aws_cloudwatch_log_group" "fluent-bit-system" {
   count             = var.fb_log_systemd ? 1 : 0
-  name              = "${local.fluentbit_system_log_name}-${module.eks.cluster_name}"
+  name              = local.fluentbit_system_log_name
   retention_in_days = var.fb_system_log_retention
   kms_key_id        = var.fb_log_encryption ? module.cloudwatch_kms.key_arn : null
   tags              = var.fb_tags
