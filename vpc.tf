212ebc63255c42a51e44aee855902b2af271f9b0
derek.howard <derek.howard@cms.hhs.gov>
2024-06-12 20:51:43 -0400

    enabled private dns for vpce

--- vpc.tf
+++ vpc.tf
@@ -54,11 +54,12 @@ data "aws_route_table" "all_private_route_tables" {
 
 # Endpoints are required for Karpenter to work properly since it is deployed to a private cluster
 resource "aws_vpc_endpoint" "ec2" {
-  vpc_id             = data.aws_vpc.vpc.id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.ec2"
-  security_group_ids = [for key, value in local.cluster_security_groups : value]
-  subnet_ids         = local.all_private_subnet_ids
-  vpc_endpoint_type  = "Interface"
+  vpc_id              = data.aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ec2"
+  security_group_ids  = [for key, value in local.cluster_security_groups : value]
+  subnet_ids          = local.all_private_subnet_ids
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
 
   tags = {
     Name = coalesce(var.vpc_endpoint_lookup_overrides, "${module.eks.cluster_name}-ec2-endpoint")
@@ -66,11 +67,12 @@ resource "aws_vpc_endpoint" "ec2" {
 }
 
 resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id             = data.aws_vpc.vpc.id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
-  security_group_ids = [for key, value in local.cluster_security_groups : value]
-  subnet_ids         = local.all_private_subnet_ids
-  vpc_endpoint_type  = "Interface"
+  vpc_id              = data.aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
+  security_group_ids  = [for key, value in local.cluster_security_groups : value]
+  subnet_ids          = local.all_private_subnet_ids
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
 
   tags = {
     Name = coalesce(var.vpc_endpoint_lookup_overrides, "${module.eks.cluster_name}-ecr-endpoint")
@@ -78,11 +80,12 @@ resource "aws_vpc_endpoint" "ecr_api" {
 }
 
 resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id             = data.aws_vpc.vpc.id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
-  security_group_ids = [for key, value in local.cluster_security_groups : value]
-  subnet_ids         = local.all_private_subnet_ids
-  vpc_endpoint_type  = "Interface"
+  vpc_id              = data.aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
+  security_group_ids  = [for key, value in local.cluster_security_groups : value]
+  subnet_ids          = local.all_private_subnet_ids
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
 
   tags = {
     Name = coalesce(var.vpc_endpoint_lookup_overrides, "${module.eks.cluster_name}-ecr-dkr-endpoint")
@@ -90,11 +93,12 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
 }
 
 resource "aws_vpc_endpoint" "eks" {
-  vpc_id             = data.aws_vpc.vpc.id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.eks"
-  security_group_ids = [for key, value in local.cluster_security_groups : value]
-  subnet_ids         = local.all_private_subnet_ids
-  vpc_endpoint_type  = "Interface"
+  vpc_id              = data.aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.eks"
+  security_group_ids  = [for key, value in local.cluster_security_groups : value]
+  subnet_ids          = local.all_private_subnet_ids
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
 
   tags = {
     Name = coalesce(var.vpc_endpoint_lookup_overrides, "${module.eks.cluster_name}-eks-endpoint")
@@ -102,11 +106,12 @@ resource "aws_vpc_endpoint" "eks" {
 }
 
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id             = data.aws_vpc.vpc.id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.s3"
-  security_group_ids = [for key, value in local.cluster_security_groups : value]
-  subnet_ids         = local.all_private_subnet_ids
-  vpc_endpoint_type  = "Interface"
+  vpc_id              = data.aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.s3"
+  security_group_ids  = [for key, value in local.cluster_security_groups : value]
+  subnet_ids          = local.all_private_subnet_ids
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
 
   tags = {
     Name = coalesce(var.vpc_endpoint_lookup_overrides, "${module.eks.cluster_name}-s3-endpoint")
@@ -114,11 +119,12 @@ resource "aws_vpc_endpoint" "s3" {
 }
 
 resource "aws_vpc_endpoint" "sts" {
-  vpc_id             = data.aws_vpc.vpc.id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.sts"
-  security_group_ids = [for key, value in local.cluster_security_groups : value]
-  subnet_ids         = local.all_private_subnet_ids
-  vpc_endpoint_type  = "Interface"
+  vpc_id              = data.aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.sts"
+  security_group_ids  = [for key, value in local.cluster_security_groups : value]
+  subnet_ids          = local.all_private_subnet_ids
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
 
   tags = {
     Name = coalesce(var.vpc_endpoint_lookup_overrides, "${module.eks.cluster_name}-sts-endpoint")
@@ -126,11 +132,12 @@ resource "aws_vpc_endpoint" "sts" {
 }
 
 resource "aws_vpc_endpoint" "ssm" {
-  vpc_id             = data.aws_vpc.vpc.id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.ssm"
-  security_group_ids = [for key, value in local.cluster_security_groups : value]
-  subnet_ids         = local.all_private_subnet_ids
-  vpc_endpoint_type  = "Interface"
+  vpc_id              = data.aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssm"
+  security_group_ids  = [for key, value in local.cluster_security_groups : value]
+  subnet_ids          = local.all_private_subnet_ids
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
 
   tags = {
     Name = coalesce(var.vpc_endpoint_lookup_overrides, "${module.eks.cluster_name}-ssm-endpoint")
@@ -138,11 +145,12 @@ resource "aws_vpc_endpoint" "ssm" {
 }
 
 resource "aws_vpc_endpoint" "sqs" {
-  vpc_id             = data.aws_vpc.vpc.id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.sqs"
-  security_group_ids = [for key, value in local.cluster_security_groups : value]
-  subnet_ids         = local.all_private_subnet_ids
-  vpc_endpoint_type  = "Interface"
+  vpc_id              = data.aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.sqs"
+  security_group_ids  = [for key, value in local.cluster_security_groups : value]
+  subnet_ids          = local.all_private_subnet_ids
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
 
   tags = {
     Name = coalesce(var.vpc_endpoint_lookup_overrides, "${module.eks.cluster_name}-sqs-endpoint")
