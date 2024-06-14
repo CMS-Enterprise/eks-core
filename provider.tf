b74b9b42d710f7c63b08cf3a250f8cee2638f840
hpalaparthi <hpalaparthi@vivsoft.io>
2024-06-14 12:17:16 -0400

    eniconfig

--- provider.tf
+++ provider.tf
@@ -11,3 +11,10 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.main.token
   }
 }
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.main.token
+  load_config_file       = false
+}
