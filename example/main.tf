f6bb1308fd8b2bfc141c109fc402b765bc788baa
Arun Sanna <asanna@vivsoft.io>
2024-06-20 20:38:29 -0400

    UPDATED: Updated variable values in example/main.tf
    - Added new variable "cluster_custom_name-eks-core " with a default value of "impl"
    - Added new variable "gold_image_date" with a default value of "2024-05"
    - variable "project" with a default value of "CMS-Enterprise"
    - Modified the "main-eks" module to use the new variables "cluster_custom_name", "env", "gold_image_date", and "project" with the corresponding variable values.

--- example/main.tf
+++ example/main.tf
@@ -7,11 +7,31 @@ provider "aws" {
   }
 }
 
+variable "cluster_custom_name" {
+  type    = string
+  default = "cms-eks-core"
+}
+
+variable "env" {
+  type    = string
+  default = "impl"
+}
+
+variable "gold_image_date" {
+  type    = string
+  default = "2024-05"
+}
+
+variable "project" {
+  type    = string
+  default = "CMS-Enterprise"
+}
+
 module "main-eks" {
   source = "../"
 
-  cluster_custom_name = "howard-test"
-  env                 = "impl"
-  gold_image_date     = "2024-05"
-  project             = "batcave"
+  cluster_custom_name = var.cluster_custom_name
+  env                 = var.env
+  gold_image_date     = var.gold_image_date
+  project             = var.project
 }
