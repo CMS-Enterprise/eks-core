0876b06aaf16aa033bd565ed9a605f0f6ec6d4a3
Arun Sanna <asanna@vivsoft.io>
2024-06-20 20:45:41 -0400

    added a todo

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,5 @@ output "container_subnets_by_zone" {
   description = "map of AZs to container subnet ids"
   value       = { for container in data.aws_subnet.container : container.availability_zone => container.id }
 }
+
+## To-Do Please add other outputs here, these will be used for the ADO infra overlay instead of using data modules.
