c22ec4c1e2bc10a935fc4581f14dbf034dabf747
Hasan Tutac <hasan.tutac@cms.hhs.gov>
2024-06-24 15:07:42 -0400

    added policy to test secret store

--- iam_policies.tf
+++ iam_policies.tf
@@ -27,4 +27,61 @@ data "aws_iam_policy_document" "vpc" {
   }
 }
 
+resource "aws_iam_role" "role" {
+  name = "secret_hasan_test"
+  path = "/delegatedadmin/developer/"
+
+  permissions_boundary = "arn:aws:iam::111594127594:policy/cms-cloud-admin/ct-ado-poweruser-permissions-boundary-policy"
+  assume_role_policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "arn:aws:iam::111594127594:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/94473F8E41C46EB15639C5BDE258482B"
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringEquals": {
+                    "oidc.eks.us-east-1.amazonaws.com/id/94473F8E41C46EB15639C5BDE258482B:aud": "sts.amazonaws.com",
+                    "oidc.eks.us-east-1.amazonaws.com/id/94473F8E41C46EB15639C5BDE258482B:sub": [
+                        "system:serviceaccount:default:app-test-secret-store-sa"
+                    ]
+                }
+            }
+        }
+    ]
+  })
+}
+
+
+# IAM policy
+resource "aws_iam_policy" "policy" {
+  name   = "secret_hasan_test"
+  path   = "/delegatedadmin/developer/"
+  policy = data.aws_iam_policy_document.policy.json
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid = "secretsdriver"
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+    ]
+    resources = [
+      "arn:aws:secretsmanager:*:*:secret:impl/*", # This configuration enables the service account to only be able to access secrets that prefix with impl/
+    ]
+    effect = "Allow"
+  }
+}
+
+
+# policy attachment
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+  
+
 
