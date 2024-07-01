35cde67b8b4749eb6255a4af43bc46769adcc52c
Derrin Chong <dchong@revacomm.com>
2024-07-01 12:48:34 -1000

    Set for EPPE2 nonprod account

--- example/main.tf
+++ example/main.tf
@@ -10,8 +10,8 @@ provider "aws" {
 module "main-eks" {
   source = "git::https://github.com/CMS-Enterprise/Energon-Kube.git?ref=3.0.1"
 
-  cluster_custom_name = "temp-test"
-  env                 = "impl"
+  cluster_custom_name = "eppe-nonprod"
+  env                 = "nonprod"
   gold_image_date     = "2024-05"
-  project             = "batcave"
+  project             = "eppe"
 }
