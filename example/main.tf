28ae70ab88c78db19eba41a1605709e23cabaa1d
Arun Sanna <asanna@vivsoft.io>
2024-07-15 22:46:05 -0400

    testing with updated provider config to assume role via jenkins

--- example/main.tf
+++ example/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 module "main-eks" {
   source              = "git@github.com:CMS-Enterprise/Energon-Kube.git?ref=arun-dev"
 
-  cluster_custom_name = "arun-test"
+  cluster_custom_name = "a-test"
   env                 = "impl"
   gold_image_date     = "2024-05"
   ado                 = "batcave"
--- provider.tf
+++ provider.tf
@@ -8,39 +8,39 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
 }
 
-provider "kubectl" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  load_config_file       = false
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
-}
-
 # provider "kubectl" {
 #   host                   = module.eks.cluster_endpoint
 #   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#     command     = "aws"
-#   }
+#   load_config_file       = false
+#   token                  = data.aws_eks_cluster_auth.cluster_auth.token
 # }
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.eks.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#       command     = "aws"
-#     }
-#   }
-# }
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    command     = "aws"
+  }
+}
 
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
   }
 }
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = module.eks.cluster_endpoint
+#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#     token                  = data.aws_eks_cluster_auth.cluster_auth.token
+#   }
+# }
