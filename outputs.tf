391a537ee3ae4efb4475212740dec56bad06a3f0
Arun Sanna <asanna@vivsoft.io>
2024-07-16 10:04:04 -0400

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,5 @@ output "container_subnets_by_zone" {
   description = "map of AZs to container subnet ids"
   value       = { for container in data.aws_subnet.container : container.availability_zone => container.id }
 }
+
+output "eks" { value = module.eks}
