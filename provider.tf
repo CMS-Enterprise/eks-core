9952e17d77fa20ce5add703fa5c22bb1462fe432
P8TF <saritpanta@VivSoftdminsMBP.lan>
2024-07-24 18:44:50 -0400

    changes in provider.tf for jenkin

--- provider.tf
+++ provider.tf
@@ -1,21 +1,17 @@
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = module.eks.eks_cluster_name
+}
 provider "kubectl" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    command     = "aws"
+    host                   = module.eks.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster_auth.token
   }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
+    host                   = module.eks.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster_auth.token
   }
 }
