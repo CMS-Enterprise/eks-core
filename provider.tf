9f4a0c7db5c06ebe36075bf2655287bb3267703f
P8TF <saritpanta@VivSoftdminsMBP.lan>
2024-07-24 18:46:50 -0400

    provider updated

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "kubectl" {
     cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster_auth.token
   }
-}
+
 
 provider "helm" {
   kubernetes {
