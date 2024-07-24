9d62698fd405adf0e3b38a6c780db9c11eb8607c
P8TF <saritpanta@VivSoftdminsMBP.lan>
2024-07-24 18:51:27 -0400

    provider updated for jenkin

--- provider.tf
+++ provider.tf
@@ -1,17 +1,17 @@
 data "aws_eks_cluster_auth" "cluster_auth" {
-  name = module.eks.eks_cluster_name
+  name = module.eks.cluster_name
 }
 provider "kubectl" {
-    host                   = module.eks.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster_auth.token
   }
 
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster_auth.token
   }
 }
