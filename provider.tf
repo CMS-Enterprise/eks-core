cdd767baa87c383b22b7f84036ab2d11b6205982
P8TF <saritpanta@VivSoftdminsMBP.lan>
2024-07-25 13:13:20 -0400

    provider testing

--- provider.tf
+++ provider.tf
@@ -2,18 +2,18 @@ data "aws_eks_cluster_auth" "cluster_auth" {
   name = module.eks.cluster_name
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
-}
-
-# provider "kubectl" {
+# provider "kubernetes" {
 #   host                   = module.eks.cluster_endpoint
 #   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   load_config_file       = false
 #   token                  = data.aws_eks_cluster_auth.cluster_auth.token
 # }
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+}
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
