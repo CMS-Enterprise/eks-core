e5a626ad959c434ebf240d09175ebc49d1166541
derek.howard <derek.howard@cms.hhs.gov>
2024-07-29 11:30:37 -0400

    update main example

--- example/main.tf
+++ example/main.tf
@@ -8,11 +8,11 @@ provider "aws" {
 }
 
 module "main-eks" {
-  source = "git@github.com:CMS-Enterprise/Energon-Kube.git?ref=3.1.1"
+  source = "git@github.com:CMS-Enterprise/Energon-Kube.git?ref=3.1.2"
 
   cluster_custom_name = "temp-test"
   env                 = "impl"
-  gold_image_date     = "2024-06"
+  gold_image_date     = "2024-07"
   ado                 = "batcave"
   program_office      = "batman"
 }
