5801c0c5b43576aeb8fb4dafd0f250d24015106e
Spencer Ho <sho@revacomm.com>
2024-07-31 09:32:53 -0700

    removing aws_eks_cluster_auth from provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
-data "aws_eks_cluster_auth" "cluster_auth" {
-  name = module.eks.cluster_name
-}
-
 provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
