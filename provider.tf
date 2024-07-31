9f7f38b04f4729a73b85724f0b8aef7d9797b012
Spencer Ho <sho@revacomm.com>
2024-07-31 09:40:33 -0700

    updating token

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   load_config_file       = false
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+  token                  = data.aws_eks_cluster_auth.main.token
 }
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+    token                  = data.aws_eks_cluster_auth.main.token
   }
 }
